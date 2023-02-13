601e284fdaa6c68d1d97a71b7cfbb01efc607857
Ryle <Ryle@outlook.ph>
2023-02-13 11:12:42 +0800

    Add project files.

---
+++ 1. Hello World.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultimate_SDPT_CSharp_Tutorial_Series
+{
+    class Hello_World
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World WITH static field");
+        }
+
+        public void HelloWorld2()
+        {
+            Console.WriteLine("Hello World WITHOUT static");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ultimate_SDPT_CSharp_Tutorial_Series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("A. Hello World with static field");
+            Console.WriteLine("B. Hello World without static field");
+            Console.WriteLine("C. Arrays");
+            Console.Write("Choose a letter to continue: ");
+            //User input: Ep3
+            //string option=Console.ReadLine();
+            //Menu(option); //another option is "Menu(console.ReadLine());"
+            Menu(Console.ReadLine().ToUpper());
+        }
+
+        //classes: ep14
+        //methods
+        static void Menu(string option)
+        {
+            //if-else
+            if (option == "A")
+            {
+                Hello_World.HelloWorld();
+                /*  Requires "static" in the following code in Hello World.cs:
+                    public static void HelloWorld()
+                    Ep14(24:11)*/
+            }
+            else if (option == "B")
+            {
+                Hello_World h = new Hello_World();
+                h.HelloWorld2();
+                //Ep14(9:48)
+            }
+            else if (option == "C")
+            {
+                Arrays.ArraysLesson();
+            }
+        }
+    }
+}
